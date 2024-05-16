e8011b1d5191709d3095be8eab3a9d7ac9a68769
josephchamakal <152864305+josephchamakal@users.noreply.github.com>
2024-05-16 16:28:42 +0300

    Add files via upload

---
+++ SmartTech.js
@@ -0,0 +1,15 @@
+const Customer = document.getElementById("Customers Reviews");
+const name = document.querySelector(".customers");
+const LeaveUs = document.querySelector(".us");
+const more = document.querySelector(".more");
+
+
+// LeaveUs.addEventListener("click",(Event)=>{
+//     Event.preventDefault();
+//     console.log("clicked");
+// })
+more.addEventListener("click",(event)=>{
+    Event.preventDefault();
+    console.log("clicked");
+
+})
